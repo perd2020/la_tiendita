9217ed250b93ea012b792c3ea0f6d594a99a6dca
Patricia Ruiz Diaz <pruizdiaz927@gmail.com>
2023-12-15 01:20:53 -0300

    cambio de archivos js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
-//lista vacia para que carge los productos
-// let productos = [];
-//que me recorra el json de productos
-// fetch("./js/productos.json")
-//     .then(response => response.json())
-//     .tehen(data => {
-//         productos = data;
-//         cargarProductos(productos);
-//     })
-//me traigo los elementos desde el HTML
+
 
 const productos=[
     {
         "id": "hombre_campera_1",
         "titulo": "Hombres 01",
-        "imagen": "/assets/img-prod/hombre/hombre-campera-1.webp",
+        "imagen": "/assets/img-prod/hombre/hombre-campera-1.png",
         "nombre": "Abrigos",
         "descripcion":"Producto de eco cuero calidad 100%, Origen Argentina",
         "categoria": {
@@ -26,7 +17,7 @@ const productos=[
     {
         "id": "hombre_campera_2",
         "titulo": "Hombres 02",
-        "imagen": "/assets/img-prod/hombre/hombre-campera-2.webp",
+        "imagen": "/assets/img-prod/hombre/hombre-campera-2.png",
         "nombre": "Abrigos",
         "descripcion":"Camperas de abrigo importadas.",
         "categoria": {
@@ -39,7 +30,7 @@ const productos=[
     {
         "id": "hombre_campera_3",
         "titulo": "Hombres 03",
-        "imagen": "./assets/img-prod/hombre/hombre-campera-3.webp",
+        "imagen": "./assets/img-prod/hombre/hombre-campera-3.jpg",
         "nombre": "Abrigos",
         "descripcion":"Chalecos de abrigo, diferentes modelos..",
         "categoria": {
@@ -52,7 +43,7 @@ const productos=[
     {
         "id": "hombre_camisas_1",
         "titulo": "Hombres 04",
-        "imagen": "/assets/img-prod/hombre/hombre-camisa-1.webp",
+        "imagen": "/assets/img-prod/hombre/hombre-camisa-1.png",
         "nombre": "camisa",
         "descripcion":"Camisas de algodon, varios colores.Todos los talles.",
         "categoria": {
@@ -65,7 +56,7 @@ const productos=[
     {
         "id": "hombre_camisas_2",
         "titulo": "Hombres 05",
-        "imagen": "/assets/img-prod/hombre/hombre-camisa-2.webp",
+        "imagen": "/assets/img-prod/hombre/hombre-camisa-2.png",
         "nombre": "camisa",
         "descripcion":"Camisas estilo urbano, entalladas confeccionadas en nylon y algodon..",
         "categoria": {
@@ -78,7 +69,7 @@ const productos=[
     {
         "id": "hombre_camisas_3",
         "titulo": "Hombres 06",
-        "imagen": "/assets/img-prod/hombre/hombre-camisa-3.webp",
+        "imagen": "/assets/img-prod/hombre/hombre-camisa-3.png",
         "nombre": "camisa",
         "descripcion":"Camisas importadas.Varios colores.Todos los talles..",
         "categoria": {
@@ -103,7 +94,7 @@ const productos=[
     {
         "id": "hombre_pantalon_2",
         "titulo": "Hombres 08",
-        "imagen": "/assets/img-prod/hombre/hombre-pantalon-2.webp",
+        "imagen": "/assets/img-prod/hombre/hombre-pantalon-2.jfif",
         "nombre": "pantalon",
         "descripcion":"pantalons estilo urbano, entalladas confeccionadas en nylon y algodon..",
         "categoria": {
@@ -116,7 +107,7 @@ const productos=[
     {
         "id": "hombre_pantalon_3",
         "titulo": "Hombres 09",
-        "imagen": "/assets/img-prod/hombre/hombre-pantalon-3.webp",
+        "imagen": "/assets/img-prod/hombre/hombre-pantalon-3.png",
         "nombre": "pantalon",
         "descripcion":"pantalones importadas.Varios colores.Todos los talles..",
         "categoria": {
@@ -129,7 +120,7 @@ const productos=[
     {
         "id": "mujer_campera_1",
         "titulo": "campera de mujer ",
-        "imagen": "/assets/img-prod/mujer/mujer-campera-1.webp",
+        "imagen": "/assets/img-prod/mujer/mujer-campera-1.jfif",
         "nombre": "Abrigos",
         "descripcion":"Camperas cortas, tipo puff, importadas . Varios colores.Desde el talle S al XXXL!!.",
         "categoria": {
@@ -142,7 +133,7 @@ const productos=[
     {
         "id": "mujer_campera_2",
         "titulo": "campera de mujer",
-        "imagen": "/assets/img-prod/mujer/mujer-campera-2.webp",
+        "imagen": "/assets/img-prod/mujer/mujer-campera-2.png",
         "nombre": "Abrigos",
         "descripcion":"Camperas de abrigo importadas.",
         "categoria": {
@@ -166,7 +157,7 @@ const productos=[
         
         {"id": "mujer_camisa_1",
         "titulo": "camisa de mujer",
-        "imagen": "/assets/img-prod/mujer/mujer-camisa-1.webp",
+        "imagen": "/assets/img-prod/mujer/mujer-camisa-1.jfif",
         "nombre": "camisa",
         "descripcion":"Camisas manga larga o 3/4 . Varios colores. Todos los talles.",
         "categoria": {
@@ -178,7 +169,7 @@ const productos=[
     
         {"id": "mujer_camisa_2",
         "titulo": "remeras de mujer",
-        "imagen": "/assets/img-prod/mujer/mujer-camiseta-5.webp",
+        "imagen": "/assets/img-prod/mujer/mujer-camiseta-5.jfif",
         "nombre": "camisa",
         "descripcion":"Talle unico.Varios colores.",
         "categoria": {
@@ -190,7 +181,7 @@ const productos=[
     
         {"id": "mujer_camisa_3",
         "titulo": "remeras de diseño",
-        "imagen": "/assets/img-prod/mujer/mujer-camiseta-3.webp",
+        "imagen": "/assets/img-prod/mujer/mujer-camiseta-3.jfif",
         "nombre": "camisa",
         "descripcion":"Tallle unico",
         "categoria": {
@@ -203,7 +194,7 @@ const productos=[
 
         {"id": "mujer_pantalon_1",
         "titulo": "Jaens de mujer",
-        "imagen": "/assets/img-prod/mujer/mujer-pantalon-1.webp",
+        "imagen": "/assets/img-prod/mujer/mujer-pantalon-1.jfif",
         "nombre": "jean rotura DENIM",
         "descripcion":"Calce perfecto.todos los talles",
         "categoria": {
@@ -215,7 +206,7 @@ const productos=[
     
         {"id": "mujer_pantalon_2",
         "titulo": "jaens chupins de mujer",
-        "imagen": "/assets/img-prod/mujer/mujer-pantalon-2.webp",
+        "imagen": "/assets/img-prod/mujer/mujer-pantalon-2.jfif",
         "nombre": "Jaen elastizado DENIM",
         "descripcion":"Todos los talles, variedad en estilos y colores.",
         "categoria": {
@@ -227,7 +218,7 @@ const productos=[
     
         {"id": "mujer_pantalon_3",
         "titulo": "mom's de mujer",
-        "imagen": "/assets/img-prod/mujer/mujer-pantalon-3.webp",
+        "imagen": "/assets/img-prod/mujer/mujer-pantalon-3.jfif",
         "nombre": "denim",
         "descripcion":"Jaen rigido acampanado con rotura  todos los , variedad en estilos y colores.",
         "categoria": {
@@ -240,7 +231,7 @@ const productos=[
     
         {"id": "ninho_campera_1",
         "titulo": "camperas para nena",
-        "imagen": "/assets/img-prod/ninhos/ninhos-campera-14.webp",
+        "imagen": "/assets/img-prod/ninhos/ninhos-campera-14.jfif",
         "nombre": "Abrigos",
         "descripcion":"este producto es de calidad premium",
         "categoria": {
@@ -252,7 +243,7 @@ const productos=[
     
         {"id": "ninho_campera_2",
         "titulo": "rompevientos",
-        "imagen": "/assets/img-prod/ninhos/ninhos-campera-11.webp",
+        "imagen": "/assets/img-prod/ninhos/ninhos-campera-11.jfif",
         "nombre": "Abrigos",
         "descripcion":"rpm´evientos de diselño. importadas.",
         "categoria": {
@@ -264,7 +255,7 @@ const productos=[
     
         {"id": "ninho_campera_3",
         "titulo": "campera para nenes",
-        "imagen": "/assets/img-prod/ninhos/ninhos-campera-5.webp",
+        "imagen": "/assets/img-prod/ninhos/ninhos-campera-5.jfif",
         "nombre": "Abrigos",
         "descripcion":"Camperas inflables importadas . Varios colores. Todos los talles.",
         "categoria": {
@@ -277,7 +268,7 @@ const productos=[
         
         {"id": "ninho_remera_1",
         "titulo": "Ninhos 04",
-        "imagen": "/assets/img-prod/ninhos/ninhos-remera-11.webp",
+        "imagen": "/assets/img-prod/ninhos/ninhos-remera-11.jfif",
         "nombre": "remera",
         "descripcion":"Remera de algodon  Variedad en colores y modelos.",
         "categoria": {
@@ -289,7 +280,7 @@ const productos=[
     
         {"id": "ninho_remera_2",
         "titulo": "Ninhos 05",
-        "imagen": "/assets/img-prod/ninhos/ninhos-remera-6.webp",
+        "imagen": "/assets/img-prod/ninhos/ninhos-remera-6.jfif",
         "nombre": "remera",
         "descripcion":"Remera de algodon  Variedad en colores y modelos .",
         "categoria": {
@@ -301,7 +292,7 @@ const productos=[
     
         {"id": "ninho_remera_3",
         "titulo": "Ninhos 06",
-        "imagen": "/assets/img-prod/ninhos/ninhos-pulover-4.webp",
+        "imagen": "/assets/img-prod/ninhos/ninhos-pulover-4.jfif",
         "nombre": "pulover",
         "descripcion":"Pulovers infantiles  Variedad en colores y modelos Talles del 1 al 14.",
         "categoria": {
@@ -314,7 +305,7 @@ const productos=[
     
         {"id": "ninho_pantalon_1",
         "titulo": "Ninhos 07",
-        "imagen": "/assets/img-prod/ninhos/ninhos-pantalon-4.webp",
+        "imagen": "/assets/img-prod/ninhos/ninhos-pantalon-4.jfif",
         "nombre": "Pantalon deportivo NENE",
         "descripcion":" Pantalon de algodon  Variedad en colores y modelos Talles del 1 al 14.s",
         "categoria": {
@@ -326,7 +317,7 @@ const productos=[
     
         {"id": "ninho_pantalon_2",
         "titulo": "Ninhos 08",
-        "imagen": "/assets/img-prod/ninhos/ninhos-pantalon-5.webp",
+        "imagen": "/assets/img-prod/ninhos/ninhos-pantalon-5.jfif",
         "nombre": "Jaens para NIÑOS",
         "descripcion":"Jaens nena o varon Variedad en colores y modelos Talles del 1 al 14..",
         "categoria": {
@@ -338,7 +329,7 @@ const productos=[
     
         {"id": "ninho_pantalon_3",
         "titulo": "Ninhos 09",
-        "imagen": "/assets/img-prod/ninhos/ninhos-pantalon-9.webp",
+        "imagen": "/assets/img-prod/ninhos/ninhos-pantalon-9.png",
         "nombre": "Pantalon babucha-PRODUCTO AGOTADO",
         "descripcion":" Pantalon de algodon puño con  elastico<br> Variedad en colores y modelos <br>Talles del 1 al 14.",
         "categoria": {
@@ -351,7 +342,7 @@ const productos=[
     
         {"id": "bebe_campera_1",
         "titulo": "Bebes 01",
-        "imagen": "/assets/img-prod/bebe/bb-campera-1.webp",
+        "imagen": "/assets/img-prod/bebe/bb-campera-1.jfif",
         "nombre": "chalecos NENE NENA",
         "descripcion":"chalecos super abrigados y cancheros.Desde el talle 0m al talle 12m.",
         "categoria": {
@@ -375,7 +366,7 @@ const productos=[
     
         {"id": "bebe_campera_3",
         "titulo": "Bebes 03",
-        "imagen": "./assets/img-prod/bebe/bb-campera-3.webp",
+        "imagen": "./assets/img-prod/bebe/bb-campera-3.jfif",
         "nombre": "Abrigos",
         "descripcion":"camperita de abrigo para bebes super canchera, desde el talle 0m al talle 12m.",
         "categoria": {
@@ -387,7 +378,7 @@ const productos=[
     
         {"id": "bebe_buzo_1",
         "titulo": "Bebes 04",
-        "imagen": "assets/img-prod/bebe/bb-buzo-1.webp",
+        "imagen": "assets/img-prod/bebe/bb-buzo-1.jfif",
         "nombre": "buzo",
         "descripcion":"bucitos para bebes de la mejor calidad ",
         "categoria": {
@@ -399,7 +390,7 @@ const productos=[
     
         {"id": "bebe_buzo_2",
         "titulo": "Bebes 05",
-        "imagen": "/assets/img-prod/bebe/bb-buzo-2.webp",
+        "imagen": "/assets/img-prod/bebe/bb-buzo-2.jfif",
         "nombre": "abrigo para bebes",
         "descripcion":"abrigos para bebes del 0m al  12m.",
         "categoria": {
@@ -423,7 +414,7 @@ const productos=[
         
         {"id": "bebe_conjunto_1",
         "titulo": "Bebes 07 ",
-        "imagen": "/assets/img-prod/bebe/bb-conjunto-1.webp",
+        "imagen": "/assets/img-prod/bebe/bb-conjunto-1.jfif",
         "nombre": "conjunto de algodon",
         "descripcion":"producto premium",
         "categoria": {
@@ -447,7 +438,7 @@ const productos=[
     
         {"id": "bebe_conjunto_3",
         "titulo": "Bebes 09",
-        "imagen": "/assets/img-prod/bebe/bb-conjunto-3.webp",
+        "imagen": "/assets/img-prod/bebe/bb-conjunto-3.jfif",
         "nombre": "denim",
         "descripcion":"conjunto de algodon y friza. del 0 m a 12m.",
         "categoria": {
@@ -459,7 +450,7 @@ const productos=[
         
         {"id": "bebe_osito_1",
         "titulo": "Bebes 10",
-        "imagen": "/assets/img-prod/bebe/bb-osito-1.webp",
+        "imagen": "/assets/img-prod/bebe/bb-osito-1.jfif",
         "nombre": "jconjunto de algodon",
         "descripcion":"conjunto de algodon, todos los talles",
         "categoria": {
@@ -483,7 +474,7 @@ const productos=[
     
         {"id": "bebe_osito_3",
         "titulo": "Bebes 12",
-        "imagen": "/assets/img-prod/bebe/bb-osito-3.webp",
+        "imagen": "/assets/img-prod/bebe/bb-osito-3.jfif",
         "nombre": "conjunto de algodon",
         "descripcion":"este producto es de calidad.",
         "categoria": {
@@ -504,7 +495,7 @@ const numerito = document.querySelector("#numerito");
 
 botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
     aside.classList.remove("aside-visible");
-}));
+}))
 
 
 function cargarProductos(productosElegidos) {
@@ -528,8 +519,10 @@ function cargarProductos(productosElegidos) {
     });
     actualizarBotonesAgregar();
 };
+//carga productos en la pagina despues de crear los div
 cargarProductos(productos); 
 
+//quiero que me carge primero esto luego de crear la pagina
 botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
 
@@ -550,38 +543,47 @@ botonesCategorias.forEach(boton => {
 });
 
 function actualizarBotonesAgregar() {
+    //que vuelva a buscar en el DOM todos los botones producto-agregar
     botonesAgregar = document.querySelectorAll(".producto-agregar");
 
     botonesAgregar.forEach(boton => {
         boton.addEventListener("click", agregarAlCarrito);
     });
-};
+}
+
+//cargando productos al local storage  
+
 
 let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
 
+//debe actualizarse el numerito del carrito
 if (productosEnCarritoLS) {
-    productosEnCarrito = JSON.parse(productosEnCarritoLS); 
+    productosEnCarrito = JSON.parse(productosEnCarritoLS); //obtener lo que tenga en el localstorage
     actualizarNumerito();
 } else {
-    productosEnCarrito = [];
-};
+    productosEnCarrito = [];//creo un array vacio como inicio de productos que van al carrito
+}
+
+
 
 function agregarAlCarrito(e) {
-    const idBoton = e.currentTarget.id; 
-    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    const idBoton = e.currentTarget.id; //guardo el id del boton agregar donde hizo click
+    const productoAgregado = productos.find(producto => producto.id === idBoton); //guarda en esta const la busqueda que coincida con lo que tengo en idBoton
 
     if(productosEnCarrito.some(producto => producto.id === idBoton)) {
-        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
-        productosEnCarrito[index].cantidad++; 
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);// buscando el index del producto
+        productosEnCarrito[index].cantidad++; //si ya esta que aumente la propiedad cantidad de 1 en 1
     } else {
-        productoAgregado.cantidad = 1; 
-        productosEnCarrito.push(productoAgregado);
+        productoAgregado.cantidad = 1; //a producto agregado le estoy creando una nueva propiedad que es cantidad:1
+        productosEnCarrito.push(productoAgregado);//agregar el nuevo producto al array de productos en carrito
     }
     
     actualizarNumerito();
 
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 }
+
+
 
 function actualizarNumerito() {
     let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
